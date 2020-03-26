903c74402c3e2c57de9037e520a6a13de3410ee1
Julie T <joules2319@gmail.com>
2020-03-26 14:36:10 -0600

    working get workouts

---
+++ routes/api-routes.js
@@ -0,0 +1,10 @@
+const db = require("../models")
+
+module.exports = function (app) {
+  app.get("/api/workouts", function (req, res) {
+    db.Workout.find({}).then(function (dbWorkouts) {
+      res.json(dbWorkouts)
+    })
+  })
+
+}
