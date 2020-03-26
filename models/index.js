c6bc9ed72a7d81514274aa5d486319b7ce52d3d7
Julie T <joules2319@gmail.com>
2020-03-26 16:47:13 -0600

    attempting lower case pathing

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 // Exporting an object containing all of our models
 const path = require("path")
 module.exports = {
-  Workout: require(path.join(__dirname, "../models/Workout.js"))
+  Workout: require("./workout.js")
 };
