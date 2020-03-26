7e4fd926a071df098b7423f23ad746692dec45a8
Julie T <joules2319@gmail.com>
2020-03-26 16:03:49 -0600

    fixed deprecation warning

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
